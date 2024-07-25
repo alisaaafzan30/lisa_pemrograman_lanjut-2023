f714e32891c5f2bf7d5c6b5f968e671d5ea469aa
alisaaafzan30 <alisaaafzannn@gmail.com>
2024-07-25 22:26:43 +0700

    Tugas UAS

---
+++ UAS Lisa/src/lisa/Main.java
@@ -0,0 +1,9 @@
+package lisa;
+import gui.*;
+public class Main {
+    public static void main(String[] args) {
+    Register register = new Register();
+    // setLocationRelative(null)
+
+    }
+}
